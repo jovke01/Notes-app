3ba45116642ab4409e50f79945b271ae83bbadde
Pavle Jovanovic <jovanovicpavle00@gmail.com>
2021-06-19 12:04:54 +0200

    post req working

--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,26 @@
+import { useState } from "react"
 
 const Create = () => {
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:8000/notes', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, body })
+        })
+    }
     return (
         <div className="create">
             <h2>Create a new Note</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Title</h3>
-                <input type="text" placeholder="Note title" />
+                <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Note title" />
                 <h3>Body</h3>
-                <textarea />
+                <textarea value={body} onChange={(e) => setBody(e.target.value)} placeholder="Note body" />
                 <button>Submit</button>
             </form>
         </div>
